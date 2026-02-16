8fd3a0755756c334bfbfa9c11aef82f07751ad4c
pepapix99-glitch <pepapix99@gmail.com>
2026-02-16 22:30:12 +0800

    multipleAssignment.py

--- multipleAssignment.py
+++ multipleAssignment.py
@@ -1,19 +1,20 @@
-#name = "Pix"
-#age = 23
-#dumdum = True
+name = "Pix"
+age = 23
+dumdum = True
 
-#print(name)
-#print(age)
-#print(dumdum)
+print(name)
+print(age)
+print(dumdum)
 
-#name, age, dumdum = "Pix", 23, True 
+name, age, dumdum = "Pix", 23, True 
 
 Pix = Hannah = Nen = 23
 
-#print(name, age, dumdum)
+print(name, age, dumdum)
 
 print(Pix)
 print(Hannah)
 print(Nen)
+
 
 #bitch wot 
